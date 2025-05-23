84ded4d6336c127109185e7cdeb6386a3d242240
Daniel Gak Anagrov <daniel@speckle.systems>
2025-05-23 18:01:47 +0100

    feat: first iteration

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,120 @@
 import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+import { Inventory } from "./main.js"
 
 describe("Default test", () => {
-  it("should work", () => {
-    const result = myFunction()
+  const yesterday = new Date("2024-01-01")
+  const today = new Date("2025-01-01")
+  const tomorrow = new Date("2026-01-01")
+  const future = new Date("2027-01-01")
 
-    expect(result).toBe(true)
+  it("adds one item to the inventory", () => {
+    const inventory = new Inventory()
+
+    inventory.add(today, "🍌")
+
+    const result = inventory.project(today)
+
+    expect(result).toBe("🍌")
+  })
+
+  it("adds multiple items to the inventory", () => {
+    const inventory = new Inventory()
+
+    inventory.add(today, "🍌")
+    inventory.add(today, "🥝")
+
+    const result = inventory.project(today)
+
+    expect(result).toBe("🍌🥝")
+  })
+
+  it("does not project items of a future date", () => {
+    const inventory = new Inventory()
+
+    inventory.add(today, "🍌")
+    inventory.add(tomorrow, "🥝")
+
+    const result = inventory.project(today)
+
+    expect(result).toBe("🍌")
+  })
+
+  it("projects items of a past date", () => {
+    const inventory = new Inventory()
+
+    inventory.add(yesterday, "🥝")
+    inventory.add(today, "🍌")
+
+    const result = inventory.project(today)
+
+    expect(result).toBe("🥝🍌")
+  })
+
+  it("handles a general usecase", () => {
+    const inventory = new Inventory()
+
+    inventory.add(today, "🍌")
+    inventory.add(today, "🥝")
+    inventory.add(tomorrow, "🍊")
+
+    expect(inventory.project(yesterday)).toBe("")
+    expect(inventory.project(today)).toBe("🍌🥝")
+    expect(inventory.project(tomorrow)).toBe("🍌🥝🍊")
+  })
+
+  it("removes a previously added item", () => {
+    const inventory = new Inventory()
+
+    inventory.add(today, "🍌");
+    inventory.remove(today, "🍌");
+
+    expect(inventory.project(today)).toBe("")
+  })
+
+  it("does not removes an item if it gets removed in the future", () => {
+    const inventory = new Inventory()
+
+    inventory.add(today, "🍌");
+    inventory.remove(tomorrow, "🍌");
+
+    expect(inventory.project(today)).toBe("🍌")
+  })
+
+  it("does not removes an item if it gets removed in the past", () => {
+    const inventory = new Inventory()
+
+    inventory.add(yesterday, "🍌");
+    inventory.remove(yesterday, "🍌");
+    inventory.add(today, "🍌");
+
+    expect(inventory.project(today)).toBe("🍌")
+  })
+
+  it("removes items from the future", () => {
+    const inventory = new Inventory()
+
+    inventory.add(today, "🍌");
+    inventory.add(today, "🥝");
+    inventory.remove(tomorrow, "🍌");
+
+    expect(inventory.project(yesterday)).toBe("")
+    expect(inventory.project(today)).toBe("🍌🥝")
+    expect(inventory.project(tomorrow)).toBe("🥝")
+  })
+
+
+  it("groups items", () => {
+    const inventory = new Inventory()
+
+    inventory.add(today, "🍌");
+    inventory.add(today, "🥝");
+    inventory.add(today, "🍊");
+    inventory.add(today, "🍎");
+
+    expect(inventory.project(future)).toBe("🍌🥝🍊🍎");
+
+    inventory.group(tomorrow, ["🥝", "🍊"]);
+
+    expect(inventory.project(future)).toBe("🍌|🥝🍊|🍎")
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,71 @@
-export function myFunction() {
-  return true
+type Operation = 'add' | 'remove' | 'group'
+
+type GroupEvent = {
+  operation: 'group'
+  date: Date
+  group: Group
+}
+
+type AddOrRemoveEvent = {
+  operation: 'add' | 'remove',
+  item: string,
+  date: Date
+}
+
+type Event = GroupEvent | AddOrRemoveEvent
+
+type Group = string[]
+
+export class Inventory {
+  private events: Event[] = []
+
+  add(date: Date, item: string) {
+    this.events.push({ operation: 'add', item, date })
+  }
+
+  remove(date: Date, item: string) {
+    this.events.push({ operation: 'remove', item, date })
+  }
+
+
+  group(date: Date, group: Group) {
+    this.events.push({ operation: 'group', group, date })
+  }
+
+  project(itemsBefore: Date) {
+    let itemsPerDate = this.events
+      .filter(({ date }) => date.getTime() <= itemsBefore.getTime())
+
+
+    const added = new Map<string, number>()
+    const removed = new Map<string, number>()
+
+    itemsPerDate.forEach((it) => {
+      if (it.operation == "remove") {
+        const prevCount = removed.get(it.item) ?? 0
+        removed.set(it.item, prevCount! + 1)
+      }
+
+      if (it.operation === "add") {
+        const prevCount = added.get(it.item) ?? 0
+        added.set(it.item, prevCount! + 1)
+      }
+    })
+
+    const items = [...added.entries()]
+      .filter(([item, addedCount]) => {
+        const removedCount = removed.get(item) ?? 0
+
+        return addedCount > removedCount
+      })
+      .map(([item]) => item)
+
+    const groupsToApply = itemsPerDate
+      .filter(({ operation }) => operation === 'group' ) as GroupEvent[]
+
+
+
+
+
+  }
 }
